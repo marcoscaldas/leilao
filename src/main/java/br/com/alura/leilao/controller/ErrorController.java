923f5d8090c1a28794290bcabe1a342936d1eb42
MarcosProfAds <seu-email@exemplo.com>
2024-10-26 09:18:20 -0300

    commit de alteracao 26102024

--- src/main/java/br/com/alura/leilao/controller/ErrorController.java
+++ src/main/java/br/com/alura/leilao/controller/ErrorController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-//***/
+//************/
 @ControllerAdvice
 public class ErrorController {
 
