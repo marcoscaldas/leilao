c90a9b82d0f37a1c2a2c9378a72c798b0b57bdad
Prof. Marcos Caldas <61627828+marcoscaldas@users.noreply.github.com>
2025-03-29 10:25:43 -0300

    commit comentario novo

--- src/main/java/br/com/alura/leilao/controller/ErrorController.java
+++ src/main/java/br/com/alura/leilao/controller/ErrorController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 //************/
+
+//*****/
 @ControllerAdvice
 public class ErrorController {
 
